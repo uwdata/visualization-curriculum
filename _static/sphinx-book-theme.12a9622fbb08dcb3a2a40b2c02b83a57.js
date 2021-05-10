0122b928e0009d99cb01e84f319701b4e0833b58
domoritz <domoritz@users.noreply.github.com>
2021-05-10 18:50:18 +0000

    deploy: d6ff961195883598c41dc63c358010c580ab520f

--- _static/sphinx-book-theme.12a9622fbb08dcb3a2a40b2c02b83a57.js
+++ _static/sphinx-book-theme.12a9622fbb08dcb3a2a40b2c02b83a57.js
@@ -9,23 +9,6 @@ var initTooltips=()=>{$(document).ready(function(){$('[data-toggle="tooltip"]').
 var initTocHide=()=>{var scrollTimeout;var throttle=200;var tocHeight=$("#bd-toc-nav").outerHeight(true)+$(".bd-toc").outerHeight(true);var hideTocAfter=tocHeight+200;var checkTocScroll=function(){var margin_content=$(".margin, .tag_margin, .full-width, .full_width, .tag_full-width, .tag_full_width, .sidebar, .tag_sidebar, .popout, .tag_popout");margin_content.each((index,item)=>{var topOffset=$(item).offset().top-$(window).scrollTop();var bottomOffset=topOffset+$(item).outerHeight(true);var topOverlaps=((topOffset>=0)&&(topOffset<hideTocAfter));var bottomOverlaps=((bottomOffset>=0)&&(bottomOffset<hideTocAfter));var removeToc=(topOverlaps||bottomOverlaps);if(removeToc&&window.pageYOffset>20){$("div.bd-toc").removeClass("show")
 return false}else{$("div.bd-toc").addClass("show")};})};var manageScrolledClassOnBody=function(){if(window.scrollY>0){document.body.classList.add("scrolled");}else{document.body.classList.remove("scrolled");}}
 $(window).on('scroll',function(){if(!scrollTimeout){scrollTimeout=setTimeout(function(){checkTocScroll();manageScrolledClassOnBody();scrollTimeout=null;},throttle);}});}
-var collapsibleListener=()=>{let expandUl=($ul)=>{if($ul.hasClass("collapse-ul")){$ul.removeClass("collapse-ul")
-$ul.next("i").removeClass("fa-chevron-down").addClass("fa-chevron-up")}}
-$elem=$("li.active")
-for(el of $elem){$ul=$(el).closest("ul")
-expandUl($ul)
-$ulChild=$(el).children("ul")
-expandUl($ulChild)
-$p=$ul.prev()
-if($p.is(".caption, .collapsible-parent")){$p.find("i").removeClass("fa-chevron-down").addClass("fa-chevron-up")}}
-$(".collapsible-parent>i, .caption.collapsible-parent").on("click",function(e){e.stopPropagation()
-$i=$(this)
-$collapsibleParent=$i.closest(".collapsible-parent")
-if($collapsibleParent.prop("tagName")=="P"){$ul=$collapsibleParent.next("ul")}else{$ul=$collapsibleParent.find("ul:first")}
-$ul.toggle(0,function(){if(!$i.is(".fas")){$i=$i.find("i")}
-if($i.hasClass("fa-chevron-up")){$i.removeClass("fa-chevron-up")
-$i.addClass("fa-chevron-down")}else{$i.removeClass("fa-chevron-down")
-$i.addClass("fa-chevron-up")}})})}
 var initThebeSBT=()=>{var title=$("div.section h1")[0]
 if(!$(title).next().hasClass("thebe-launch-button")){$("<button class='thebe-launch-button'></button>").insertAfter($(title))}
 initThebe();}
@@ -33,4 +16,3 @@ sbRunWhenDOMLoaded(initTooltips)
 sbRunWhenDOMLoaded(initTriggerNavBar)
 sbRunWhenDOMLoaded(scrollToActive)
 sbRunWhenDOMLoaded(initTocHide)
-sbRunWhenDOMLoaded(collapsibleListener)
